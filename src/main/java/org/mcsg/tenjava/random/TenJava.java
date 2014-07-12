826448bf1b76b87d00f0d4dc6846d2742beb8144
Double0negative <Double0negative@mc-sg.org>
2014-07-12 10:43:04 -0400

    Refactor, Create event interfaces

---
+++ src/main/java/org/mcsg/tenjava/random/TenJava.java
@@ -0,0 +1,15 @@
+package org.mcsg.tenjava.random;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TenJava extends JavaPlugin {
+	
+	public void onEnable(){
+		
+	}
+	
+	public void onDisable(){
+		
+	}
+	
+}
